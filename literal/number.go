c5d8eb9e30ad0764d0d3233ce119a223da5b8440
Hajime Hoshi <hajimehoshi@gmail.com>
2018-09-01 02:21:12 +0900

    literal: Add ReadNumber

---
+++ literal/number.go
@@ -0,0 +1,215 @@
+// Copyright 2018 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package literal
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+
+	"github.com/hajimehoshi/goc/ctype"
+)
+
+func isDigit(c byte) bool {
+	return '0' <= c && c <= '9'
+}
+
+func isIdentFirstChar(c byte) bool {
+	if 'A' <= c && c <= 'Z' {
+		return true
+	}
+	if 'a' <= c && c <= 'z' {
+		return true
+	}
+	if c == '_' {
+		return true
+	}
+	return false
+}
+
+func isIdentChar(c byte) bool {
+	if isIdentFirstChar(c) {
+		return true
+	}
+	if isDigit(c) {
+		return true
+	}
+	return false
+}
+
+type IntegerSuffix int
+
+const (
+	IntegerSuffixNone IntegerSuffix = iota
+	IntegerSuffixL
+	IntegerSuffixLL
+	IntegerSuffixU
+	IntegerSuffixUL
+	IntegerSuffixULL
+)
+
+func ReadIntegerSuffix(src *bufio.Reader) (IntegerSuffix, error) {
+	bs, err := src.Peek(3)
+	if err != nil && err != io.EOF {
+		return 0, err
+	}
+	s := ""
+	for _, b := range bs {
+		if !isIdentFirstChar(b) {
+			break
+		}
+		s += string(b)
+	}
+
+	switch s {
+	case "":
+		return IntegerSuffixNone, nil
+	case "l", "L":
+		src.Discard(1)
+		return IntegerSuffixL, nil
+	case "ll", "LL":
+		src.Discard(2)
+		return IntegerSuffixLL, nil
+	case "u", "U":
+		src.Discard(1)
+		return IntegerSuffixU, nil
+	case "ul", "UL":
+		src.Discard(2)
+		return IntegerSuffixUL, nil
+	case "ull", "ULL":
+		src.Discard(3)
+		return IntegerSuffixULL, nil
+	}
+
+	return 0, fmt.Errorf("literal: unexpected suffix %q", s)
+}
+
+type Number interface{}
+
+func ReadNumber(src *bufio.Reader) (Number, error) {
+	b, err := src.ReadByte()
+	if err != nil {
+		if err == io.EOF {
+			return nil, fmt.Errorf("literal: unexpected EOF")
+		}
+		return nil, err
+	}
+
+	// TODO: Float number
+
+	if !isDigit(b) {
+		return nil, fmt.Errorf("literal: non-digit character")
+	}
+
+	v := int64(0)
+
+	if b == '0' {
+		bs, err := src.Peek(1)
+		if err != nil && err != io.EOF {
+			return nil, err
+		}
+		if len(bs) < 1 {
+			return ctype.Int(0), nil
+		}
+		if bs[0] == 'x' || bs[0] == 'X' {
+			src.Discard(1)
+			for {
+				bs, err := src.Peek(1)
+				if err != nil && err != io.EOF {
+					return nil, err
+				}
+				if len(bs) < 1 {
+					break
+				}
+				if !isHexDigit(bs[0]) {
+					break
+				}
+				src.Discard(1)
+				v *= 16
+				v += int64(hex(bs[0]))
+			}
+		}
+		if isDigit(bs[0]) {
+			for {
+				bs, err := src.Peek(1)
+				if err != nil && err != io.EOF {
+					return nil, err
+				}
+				if len(bs) < 1 {
+					break
+				}
+				if !isDigit(bs[0]) {
+					break
+				}
+				if !isOctDigit(bs[0]) {
+					return nil, fmt.Errorf("literal: malformed octal constant")
+				}
+				src.Discard(1)
+				v *= 8
+				v += int64(bs[0] - '0')
+			}
+		}
+	} else {
+		v = int64(b - '0')
+		for {
+			bs, err := src.Peek(1)
+			if err != nil && err != io.EOF {
+				return nil, err
+			}
+			if len(bs) < 1 {
+				return ctype.Int(v), nil
+			}
+			if !isDigit(bs[0]) {
+				break
+			}
+			src.Discard(1)
+			v *= 10
+			v += int64(bs[0] - '0')
+		}
+	}
+
+	s, err := ReadIntegerSuffix(src)
+	if err != nil {
+		return nil, err
+	}
+	switch s {
+	case IntegerSuffixNone:
+		if v >= 0x80000000 {
+			return ctype.LongLong(v), nil
+		}
+		return ctype.Int(v), nil
+	case IntegerSuffixL:
+		if v >= 0x80000000 {
+			return ctype.LongLong(v), nil
+		}
+		return ctype.Long(v), nil
+	case IntegerSuffixLL:
+		return ctype.LongLong(v), nil
+	case IntegerSuffixU:
+		if v >= 0x100000000 {
+			return ctype.ULongLong(v), nil
+		}
+		return ctype.UInt(v), nil
+	case IntegerSuffixUL:
+		if v >= 0x100000000 {
+			return ctype.ULongLong(v), nil
+		}
+		return ctype.ULong(v), nil
+	case IntegerSuffixULL:
+		return ctype.ULongLong(v), nil
+	default:
+		panic("not reached")
+	}
+}
---
+++ literal/number_test.go
@@ -0,0 +1,145 @@
+// Copyright 2018 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package literal_test
+
+import (
+	"bytes"
+	"bufio"
+	"testing"
+
+	"github.com/hajimehoshi/goc/ctype"
+	. "github.com/hajimehoshi/goc/literal"
+)
+
+func TestReadIntegerSuffix(t *testing.T) {
+	cases := []struct {
+		In  string
+		Out IntegerSuffix
+		Err bool
+	}{
+		{``, IntegerSuffixNone, false},
+		{`+`, IntegerSuffixNone, false},
+		{`l`, IntegerSuffixL, false},
+		{`ll`, IntegerSuffixLL, false},
+		{`u`, IntegerSuffixU, false},
+		{`ul`, IntegerSuffixUL, false},
+		{`ull`, IntegerSuffixULL, false},
+		{`L`, IntegerSuffixL, false},
+		{`LL`, IntegerSuffixLL, false},
+		{`U`, IntegerSuffixU, false},
+		{`UL`, IntegerSuffixUL, false},
+		{`ULL`, IntegerSuffixULL, false},
+		{`l+`, IntegerSuffixL, false},
+		{`ll-`, IntegerSuffixLL, false},
+		{`u*`, IntegerSuffixU, false},
+		{`ul/`, IntegerSuffixUL, false},
+		{`ull `, IntegerSuffixULL, false},
+		{`lL`, 0, true},
+		{`Ll`, 0, true},
+		{`ulL`, 0, true},
+		{`uLl`, 0, true},
+		{`Ull`, 0, true},
+		{`la`, 0, true},
+		{`l_`, 0, true},
+	}
+	for _, c := range cases {
+		got, err := ReadIntegerSuffix(bufio.NewReader(bytes.NewReader([]byte(c.In))))
+		if err != nil && !c.Err {
+			t.Errorf("ReadIntegerSuffix(%q) should not return error but did: %v", c.In, err)
+		}
+		if err == nil && c.Err {
+			t.Errorf("ReadIntegerSuffix(%q) should return error but not", c.In)
+		}
+		if got != c.Out {
+			t.Errorf("ReadIntegerSuffix(%q): got: %d, want: %d", c.In, got, c.Out)
+		}
+	}
+}
+
+
+func TestReadNumber(t *testing.T) {
+	cases := []struct {
+		In  string
+		Out interface{}
+		Err bool
+	}{
+		{`0`, ctype.Int(0), false},
+		{`00`, ctype.Int(0), false},
+		{`000u`, ctype.UInt(0), false},
+
+		{`1`, ctype.Int(1), false},
+		{`123`, ctype.Int(123), false},
+		{`987654321`, ctype.Int(987654321), false},
+		{`1l`, ctype.Long(1), false},
+		{`16777216ULL`, ctype.ULongLong(16777216), false},
+		{`42+`, ctype.Int(42), false},
+		{`141421356ul-`, ctype.ULong(141421356), false},
+
+		// Oct
+		{`0377`, ctype.Int(255), false},
+		{`04444ll`, ctype.LongLong(2340), false},
+		{`0?`, ctype.Int(0), false},
+
+		// Hex
+		{`0x0`, ctype.Int(0), false},
+		{`0x0000`, ctype.Int(0), false},
+		{`0xdeadbeefUL`, ctype.ULong(0xdeadbeef), false},
+		{`0xffff`, ctype.Int(65535), false},
+		{`0Xffff`, ctype.Int(65535), false},
+		{`0xFFFF`, ctype.Int(65535), false},
+		{`0XFFFF`, ctype.Int(65535), false},
+
+		// Limit
+		{`0x7fffffff`, ctype.Int(0x7fffffff), false},
+		{`0x7fffffffl`, ctype.Long(0x7fffffff), false},
+		{`0x7fffffffll`, ctype.LongLong(0x7fffffff), false},
+		{`0x7fffffffu`, ctype.UInt(0x7fffffff), false},
+		{`0x7ffffffful`, ctype.ULong(0x7fffffff), false},
+		{`0x7fffffffull`, ctype.ULongLong(0x7fffffff), false},
+		{`0x80000000`, ctype.LongLong(0x80000000), false},
+		{`0x80000000l`, ctype.LongLong(0x80000000), false},
+		{`0x80000000ll`, ctype.LongLong(0x80000000), false},
+		{`0x80000000u`, ctype.UInt(0x80000000), false},
+		{`0x80000000ul`, ctype.ULong(0x80000000), false},
+		{`0x80000000ull`, ctype.ULongLong(0x80000000), false},
+		{`0xffffffff`, ctype.LongLong(0xffffffff), false},
+		{`0xffffffffl`, ctype.LongLong(0xffffffff), false},
+		{`0xffffffffll`, ctype.LongLong(0xffffffff), false},
+		{`0xffffffffu`, ctype.UInt(0xffffffff), false},
+		{`0xfffffffful`, ctype.ULong(0xffffffff), false},
+		{`0xffffffffull`, ctype.ULongLong(0xffffffff), false},
+		{`0x100000000`, ctype.LongLong(0x100000000), false},
+		{`0x100000000l`, ctype.LongLong(0x100000000), false},
+		{`0x100000000ll`, ctype.LongLong(0x100000000), false},
+		{`0x100000000u`, ctype.ULongLong(0x100000000), false},
+		{`0x100000000ul`, ctype.ULongLong(0x100000000), false},
+		{`0x100000000ull`, ctype.ULongLong(0x100000000), false},
+
+		{`08`, nil, true},
+		{`x`, nil, true},
+	}
+	for _, c := range cases {
+		got, err := ReadNumber(bufio.NewReader(bytes.NewReader([]byte(c.In))))
+		if err != nil && !c.Err {
+			t.Errorf("ReadNumber(%q) should not return error but did: %v", c.In, err)
+		}
+		if err == nil && c.Err {
+			t.Errorf("ReadNumber(%q) should return error but not", c.In)
+		}
+		if got != c.Out {
+			t.Errorf("ReadNumber(%q): got: %[2]d (%[2]T), want: %[3]d (%[3]T)", c.In, got, c.Out)
+		}
+	}
+}
