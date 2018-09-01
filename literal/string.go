e9302826c252ebb677586efb8d17bba9ddebd026
Hajime Hoshi <hajimehoshi@gmail.com>
2018-09-02 01:24:08 +0900

    literal: Add ReadString

---
+++ literal/string.go
@@ -0,0 +1,56 @@
+// Copyright 2018 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package literal
+
+import (
+	"bufio"
+	"fmt"
+)
+
+func ReadString(src *bufio.Reader) (string, error) {
+	if err := shouldRead(src, '"'); err != nil {
+		return "", err
+	}
+
+	bs := []byte{}
+loop:
+	for {
+		b, err := shouldPeekByte(src)
+		if err != nil {
+			return "", err
+		}
+		switch b {
+		case '"':
+			src.Discard(1)
+			break loop
+		case '\\':
+			b, err := ReadEscapedChar(src)
+			if err != nil {
+				return "", err
+			}
+			if b > 255 {
+				return "", fmt.Errorf("literal: not implemented")
+			}
+			bs = append(bs, byte(b))
+			continue loop
+		case '\r', '\n':
+			return "", fmt.Errorf("literal: newline in string")
+		}
+		src.Discard(1)
+		bs = append(bs, b)
+	}
+
+	return string(bs), nil
+}
---
+++ literal/string_test.go
@@ -0,0 +1,63 @@
+// Copyright 2018 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package literal_test
+
+import (
+	"bytes"
+	"bufio"
+	"testing"
+
+	. "github.com/hajimehoshi/goc/literal"
+)
+
+
+func TestReadString(t *testing.T) {
+	cases := []struct {
+		In  string
+		Out string
+		Err bool
+	}{
+		{`""`, "", false},
+		{`""+`, "", false},
+		{`"a"`, "a", false},
+		{`"xyz"`, "xyz", false},
+		{`"The quick brown fox jumps over the lazy dog."`, "The quick brown fox jumps over the lazy dog.", false},
+		{`"\\\\"`, "\\\\", false},
+		{`"\n"`, "\n", false},
+		{`"\"\""`, "\"\"", false},
+		{`"'"`, "'", false},
+		{`"\'"`, "'", false},
+		{`"\00"`, string(0), false},
+		{`"\08"`, string(0) + "8", false},
+		{`"\xff"`, string([]byte{0xff}), false},
+
+		{"\"\n\"", "", true},
+		{`"`, "", true},
+		{`"'`, "", true},
+		{`"a`, "", true},
+	}
+	for _, c := range cases {
+		got, err := ReadString(bufio.NewReader(bytes.NewReader([]byte(c.In))))
+		if err != nil && !c.Err {
+			t.Errorf("ReadString(%q) should not return error but did: %v", c.In, err)
+		}
+		if err == nil && c.Err {
+			t.Errorf("ReadString(%q) should return error but not", c.In)
+		}
+		if got != c.Out {
+			t.Errorf("ReadString(%q): got: %q, want: %q", c.In, got, c.Out)
+		}
+	}
+}
