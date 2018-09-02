9cace4f60723bdf7bcdf955592612b43e0909d87
Hajime Hoshi <hajimehoshi@gmail.com>
2018-09-02 16:53:13 +0900

    Add token

---
+++ token/token_test.go
@@ -0,0 +1,121 @@
+// Copyright 2018 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package token_test
+
+import (
+	"bytes"
+	"fmt"
+
+	. "github.com/hajimehoshi/goc/token"
+)
+
+func outputTokens(src string) {
+	tokens, err := Tokenize(bytes.NewReader([]byte(src)))
+	if err != nil {
+		fmt.Println("error")
+		return
+	}
+	for _, t := range tokens {
+		fmt.Println(t)
+	}
+}
+
+func ExampleTokenizeEmpty() {
+	outputTokens("")
+	// Output:
+}
+
+func ExampleTokenizeNumberSign() {
+	outputTokens("#")
+	// Output:
+	// #
+}
+
+func ExampleTokenizeBackslash() {
+	outputTokens("\\")
+	// Output:
+	// error
+}
+
+func ExampleTokenizeCalc() {
+	outputTokens("1+1=2")
+	// Output:
+	// number: 1 (int)
+	// +
+	// number: 1 (int)
+	// =
+	// number: 2 (int)
+}
+
+func ExampleTokenizeStrings() {
+	outputTokens(`"a""b""c"`)
+	// Output:
+	// string: "abc"
+}
+
+func ExampleTokenizeHelloWorld() {
+	outputTokens(`int main() {
+  printf("Hello, World!\n");
+  return 0;
+}`)
+	// Output:
+	// int
+	// ident: main
+	// (
+	// )
+	// {
+	// ident: printf
+	// (
+	// string: "Hello, World!\n"
+	// )
+	// ;
+	// return
+	// number: 0 (int)
+	// ;
+	// }
+}
+
+func ExampleTokenizeNewLines() {
+	outputTokens(`foo \
+bar`)
+	// Output:
+	// ident: foo
+	// ident: bar
+}
+
+func ExampleTokenizeBackslashNewLine() {
+	outputTokens(`i\
+f ("foo\
+bar") el\
+se
+\
+`)
+	// Output:
+	// if
+	// (
+	// string: "foobar"
+	// )
+	// else
+}
+
+func ExampleTokenizeInc() {
+	outputTokens(`c+++++c`)
+	// Output:
+	// ident: c
+	// ++
+	// ++
+	// +
+	// ident: c
+}
---
+++ token/tokentype.go
@@ -0,0 +1,260 @@
+// Copyright 2018 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package token
+
+import (
+	"fmt"
+)
+
+type TokenType int
+
+const (
+	NumberLiteral TokenType = iota + 128
+	StringLiteral
+	Ident
+
+	Auto
+	Bool
+	Break
+	Case
+	Char
+	Complex
+	Const
+	Continue
+	Default
+	Do
+	Double
+	Else
+	Enum
+	Extern
+	Float
+	For
+	Goto
+	If
+	Imaginary
+	Inline
+	Int
+	Long
+	Register
+	Restrict
+	Return
+	Short
+	Signed
+	Sizeof
+	Static
+	Struct
+	Switch
+	Typedef
+	Union
+	Unsigned
+	Void
+	Volatile
+	While
+
+	AddEq // +=
+	SubEq // -=
+	MulEq // *=
+	DivEq // /=
+	ModEq // %=
+	ShlEq // <<=
+	ShrEq // >>=
+	AndEq // &=
+	XorEq // ^=
+	OrEq  // |=
+
+	Arrow  // ->
+	Inc    // ++
+	Dec    // --
+	Shl    // <<
+	Shr    // >>
+	Le     // <=
+	Ge     // >=
+	Eq     // ==
+	Ne     // !=
+	AndAnd // &&
+	OrOr   // ||
+)
+
+func (t TokenType) String() string {
+	switch t {
+	case NumberLiteral:
+		return "(number)"
+	case StringLiteral:
+		return "(string)"
+	case Ident:
+		return "(ident)"
+	case Auto:
+		return "auto"
+	case Bool:
+		return "_Bool"
+	case Break:
+		return "break"
+	case Case:
+		return "case"
+	case Char:
+		return "char"
+	case Complex:
+		return "_Complex"
+	case Const:
+		return "const"
+	case Continue:
+		return "continue"
+	case Default:
+		return "default"
+	case Do:
+		return "do"
+	case Double:
+		return "double"
+	case Else:
+		return "else"
+	case Enum:
+		return "enum"
+	case Extern:
+		return "extern"
+	case Float:
+		return "float"
+	case For:
+		return "fot"
+	case Goto:
+		return "goto"
+	case If:
+		return "if"
+	case Imaginary:
+		return "_Imaginary"
+	case Inline:
+		return "inline"
+	case Int:
+		return "int"
+	case Long:
+		return "long"
+	case Register:
+		return "register"
+	case Restrict:
+		return "restrict"
+	case Return:
+		return "return"
+	case Short:
+		return "short"
+	case Signed:
+		return "signed"
+	case Sizeof:
+		return "sizeof"
+	case Static:
+		return "static"
+	case Struct:
+		return "struct"
+	case Switch:
+		return "switch"
+	case Typedef:
+		return "typedef"
+	case Union:
+		return "union"
+	case Unsigned:
+		return "unsigned"
+	case Void:
+		return "void"
+	case Volatile:
+		return "volatile"
+	case While:
+		return "while"
+	case AddEq:
+		return "+="
+	case SubEq:
+		return "-="
+	case MulEq:
+		return "*="
+	case DivEq:
+		return "/="
+	case ModEq:
+		return "%="
+	case ShlEq:
+		return "<<="
+	case ShrEq:
+		return ">>="
+	case AndEq:
+		return "&="
+	case XorEq:
+		return "^="
+	case OrEq:
+		return "|="
+	case Arrow:
+		return "->"
+	case Inc:
+		return "++"
+	case Dec:
+		return "--"
+	case Shl:
+		return "<<"
+	case Shr:
+		return ">>"
+	case Le:
+		return "<="
+	case Ge:
+		return ">="
+	case Eq:
+		return "=="
+	case Ne:
+		return "!="
+	case AndAnd:
+		return "&&"
+	case OrOr:
+		return "||"
+	default:
+		if 0 <= t && t <= 127 {
+			return string(t)
+		}
+		return fmt.Sprintf("invalid: %d", t)
+	}
+}
+
+var keywordTokenTypes = map[string]TokenType{
+	"auto":       Auto,
+	"_Bool":      Bool,
+	"break":      Break,
+	"case":       Case,
+	"char":       Char,
+	"_Complex":   Complex,
+	"const":      Const,
+	"continue":   Continue,
+	"default":    Default,
+	"do":         Do,
+	"double":     Double,
+	"else":       Else,
+	"enum":       Enum,
+	"extern":     Extern,
+	"float":      Float,
+	"for":        For,
+	"goto":       Goto,
+	"if":         If,
+	"_Imaginary": Imaginary,
+	"inline":     Inline,
+	"int":        Int,
+	"long":       Long,
+	"register":   Register,
+	"restrict":   Restrict,
+	"return":     Return,
+	"short":      Short,
+	"signed":     Signed,
+	"sizeof":     Sizeof,
+	"static":     Static,
+	"struct":     Struct,
+	"switch":     Switch,
+	"typeof":     Typedef,
+	"union":      Union,
+	"unsigned":   Unsigned,
+	"void":       Void,
+	"volatile":   Volatile,
+	"while":      While,
+}
