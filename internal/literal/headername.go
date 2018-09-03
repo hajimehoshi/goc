99a092ee5c1cbe5e65b679b12edbcab88bf342e6
Hajime Hoshi <hajimehoshi@gmail.com>
2018-09-03 19:37:45 +0900

    literal: Add ReadHeaderName

---
+++ internal/literal/headername.go
@@ -0,0 +1,57 @@
+// Copyright 2018 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package literal
+
+import (
+	"bufio"
+	"fmt"
+
+	"github.com/hajimehoshi/goc/internal/ioutil"
+)
+
+func ReadHeaderName(src *bufio.Reader) (string, error) {
+	b, err := ioutil.ShouldReadByte(src)
+	if err != nil {
+		return "", err
+	}
+	if b != '<' && b != '"' {
+		return "", fmt.Errorf("literal: '<' or '\"' expected but %q", string(b))
+	}
+
+	bs := []byte{}
+	end := byte(0)
+	switch b {
+	case '<':
+		end = '>'
+	case '"':
+		end = '"'
+	default:
+		panic("not reached")
+	}
+	for {
+		b, err := ioutil.ShouldReadByte(src)
+		if err != nil {
+			return "", err
+		}
+		switch b {
+		case end:
+			return string(bs), nil		
+		case '\n':
+			return "", fmt.Errorf("literal: unterminated header-name")
+		default:
+			bs = append(bs, b)
+		}
+	}
+}
---
+++ internal/literal/headername_test.go
@@ -0,0 +1,58 @@
+// Copyright 2018 Hajime Hoshi
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package literal_test
+
+import (
+	"bufio"
+	"bytes"
+	"testing"
+
+	. "github.com/hajimehoshi/goc/internal/literal"
+)
+
+func TestReadHeaderName(t *testing.T) {
+	cases := []struct {
+		In  string
+		Out string
+		Err bool
+	}{
+		{`""`, ``, false},
+		{`"hi"`, `hi`, false},
+		{`"h\i"`, `h\i`, false},
+		{`"\"`, `\`, false},
+		{`"\\"`, `\\`, false},
+		{`<>`, ``, false},
+		{`<\>`, `\`, false},
+		{`<\\>`, `\\`, false},
+		{`<hi>`, `hi`, false},
+		{`<h\i>`, `h\i`, false},
+		{`<<<<>`, `<<<`, false},
+
+		{`"`, "", true},
+		{`<`, "", true},
+	}
+	for _, c := range cases {
+		got, err := ReadHeaderName(bufio.NewReader(bytes.NewReader([]byte(c.In))))
+		if err != nil && !c.Err {
+			t.Errorf("ReadHeaderName(%q) should not return error but did: %v", c.In, err)
+		}
+		if err == nil && c.Err {
+			t.Errorf("ReadHeaderName(%q) should return error but not", c.In)
+		}
+		if got != c.Out {
+			t.Errorf("ReadHeaderName(%q): got: %q, want: %q", c.In, got, c.Out)
+		}
+	}
+}
